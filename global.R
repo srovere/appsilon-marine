5598e77fdc4a8243692dcc825071fd973bb29ff2
Santiago Rovere <srovere@gmail.com>
2021-08-03 13:28:32 -0300

    Code rearrangement

--- global.R
+++ global.R
@@ -10,14 +10,6 @@ require(yaml)
 # Read configuration file
 config <- yaml::yaml.load_file("configuration.yml")
 
-# Load classes for data handling
-source(file = "../DataSource.R", echo = FALSE)
-source(file = "../CSVDataSource.R", echo = FALSE)
-source(file = "../Facade.R", echo = FALSE)
-source(file = "../ShipFacade.R", echo = FALSE)
-source(file = "../ShipTypeFacade.R", echo = FALSE)
-source(file = "../LocationFacade.R", echo = FALSE)
-
 # Create instance for data source
 dataSource <- DataSource$new()$getInstance(driver = config$data_source$driver, 
                                            parameters = config$data_source$parameters)
@@ -26,6 +18,3 @@ dataSource <- DataSource$new()$getInstance(driver = config$data_source$driver,
 shipTypeFacade <- ShipTypeFacade$new(dataSource)
 shipFacade <- ShipFacade$new(dataSource)
 locationFacade <- LocationFacade$new(dataSource)
-
-# Load modules
-source(file = "modules/inputs.R", echo = FALSE)
